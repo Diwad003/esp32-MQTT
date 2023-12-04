fe5a0458e36b837bda23e2b49c5f959f70592977
Diwad003 <48996015+Diwad003@users.noreply.github.com>
2023-12-04 13:51:04 +0100

    Add files via upload

---
+++ ESP-32.ino
@@ -0,0 +1,168 @@
+#include <ArduinoMqttClient.h>
+#include <WiFiClientSecure.h>
+//#include <esp_task_wdt.h>      // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/misc_system_api.html
+#include "esp_intr_types.h"
+#include "HardwareSerial.h"
+#include "esp_rom_gpio.h"¨
+
+
+
+
+void setup();
+void loop();
+void connectToWiFi();
+void mqttConnect();
+void write_mqtt(MqttClient mqttClient, char* topic);
+void read_mqtt(MqttClient mqttClient);
+
+
+void uart_init();
+void uart_Tx(const char data);
+void uart_Tx_string(const char* data);
+byte uart_Rx();
+void uart_Rx_string(char *uart_Rx_string);
+
+  
+char ssid[] = "iPSK-UMU";    // your network SSID (name)
+char password[] = "HejHopp!!";    // your network password (use for WPA, or use as key for WEP)
+
+WiFiClient wifiClient;
+
+
+MqttClient mqttClient(wifiClient);
+const char broker[] = "tfe.iotwan.se";
+int        port     = 1883;
+char* sensor_topic  = "JD/sensor";
+char* joystick_topic = "JD/joystick"; 
+#define mqtt_username "intro23"
+#define mqtt_password "outro"
+
+
+#define Rx 16
+#define Tx 17
+
+void setup() 
+{
+  Serial.begin(38400);
+  
+  uart_init();
+  connectToWiFi();
+  mqttConnect();
+  mqttClient.subscribe(joystick_topic);
+
+  uart_Tx_string("Setup Done");
+}
+
+void loop()
+{
+  //mqttClient.poll();
+
+
+  read_mqtt(mqttClient);
+  write_mqtt(mqttClient, sensor_topic);
+  if(WiFi.status() != WL_CONNECTED) 
+  {
+    connectToWiFi();
+    mqttConnect();
+  }
+}
+
+void connectToWiFi()
+{
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to WiFi SSID: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    // wait 1 second for re-trying
+    delay(1000);
+  }
+
+  Serial.println("You're connected to the network\n");
+}
+
+void mqttConnect()
+{  
+  //Connect to MQTT broker
+  mqttClient.setUsernamePassword(mqtt_username, mqtt_password);
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) 
+  {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!\n");
+}
+
+void write_mqtt(MqttClient mqttClient, char* topic)
+{
+  char uartRX = uart_Rx();
+  if(uartRX != 0)
+  {
+    mqttClient.beginMessage(topic);
+    mqttClient.print(uartRX);
+    mqttClient.endMessage();
+
+    Serial.println(uartRX);
+  }
+}
+
+void read_mqtt(MqttClient mqttClient)
+{
+  int messageSize = mqttClient.parseMessage();
+  if (messageSize) 
+  {
+    
+    byte message;
+    while (mqttClient.available()) 
+    {
+       message = mqttClient.read();
+       //Serial.print(message);
+       uart_Tx(message);
+    }
+    uart_Tx('&');
+  }
+}
+
+void uart_init()
+{
+  Serial1.begin(38400, SERIAL_8N1, Rx, Tx);
+}
+
+void uart_Tx(const char data)
+{
+  Serial1.print(data);
+  Serial1.flush();
+
+  
+  Serial.print(data);
+}
+
+void uart_Tx_string(const char* data)
+{
+  for(int i = 0; i<strlen(data); i++)
+  {
+    uart_Tx(data[i]);
+  }
+}
+
+byte uart_Rx()
+{
+  byte incomingByte = 0;
+  if (Serial1.available() > 0) 
+  {
+    incomingByte = Serial1.read();
+  }
+
+  return incomingByte;
+}
+
